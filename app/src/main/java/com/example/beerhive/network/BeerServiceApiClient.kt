168097be8efd73464fd2bcf6b46b2e45203536e9
Mossco MacCintosh <mosscomoabelo@gmail.com>
2020-05-09 19:58:28 +0200

    Add Couroutine call adapter for network calls

--- app/src/main/java/com/example/beerhive/network/BeerServiceApiClient.kt
+++ app/src/main/java/com/example/beerhive/network/BeerServiceApiClient.kt
@@ -2,6 +2,7 @@ package com.example.beerhive.network
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,6 +11,7 @@ private const val baseUrl = "https://api.punkapi.com/v2/"
 private val gson: Gson = GsonBuilder().create()
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create(gson))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .baseUrl(baseUrl)
         .build()
 
