9f43720dad1acfac36ea150c09cd3b8f7112bd24
Mossco MacCintosh <mosscomoabelo@gmail.com>
2020-05-14 22:04:37 +0200

    Refactor beer list adapter to use data binding

--- app/src/main/java/com/example/beerhive/beerlist/BeerListAdapter.kt
+++ app/src/main/java/com/example/beerhive/beerlist/BeerListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.beerhive.beerlist
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -8,19 +9,19 @@ import com.example.beerhive.R
 import com.example.beerhive.beerlist.BeerListAdapter.BeerListViewHolder
 import com.example.beerhive.databinding.BeerItemLayoutBinding
 import com.example.beerhive.domain.Beer
+import kotlinx.android.synthetic.main.beer_item_layout.view.*
 
 class BeerListAdapter(private val beerItemClickListener: BeerItemClickListener,
                       private val beersList: List<Beer>) : RecyclerView.Adapter<BeerListViewHolder>() {
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeerListViewHolder {
-        val binding: BeerItemLayoutBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
-                R.layout.beer_item_layout, parent, false)
-        return BeerListViewHolder(binding, beerItemClickListener)
+        return BeerListViewHolder.fromParent(parent)
     }
 
     override fun onBindViewHolder(holder: BeerListViewHolder, position: Int) {
-        val beerResponse = beersList[position]
-        holder.bindData(beerResponse)
-        holder.setBeerResponse(beerResponse)
+        val domainBeer = beersList[position]
+        holder.bindData(domainBeer)
+        holder.itemView.beerCardView.setOnClickListener { beerItemClickListener.onBeerItemClicked(domainBeer) }
     }
 
     override fun getItemCount(): Int {
@@ -29,23 +30,25 @@ class BeerListAdapter(private val beerItemClickListener: BeerItemClickListener,
 
     interface BeerItemClickListener {
         fun onBeerItemClicked(domainBeer: Beer)
+
     }
 
-    class BeerListViewHolder(private var binding: BeerItemLayoutBinding,
-                             private val beerItemClickListener: BeerItemClickListener) : RecyclerView.ViewHolder(binding.root) {
+    class BeerListViewHolder private constructor(private val view: View) : RecyclerView.ViewHolder(view) {
         private lateinit var domainBeer: Beer
-        fun setBeerResponse(domainBeer: Beer) {
-            this.domainBeer = domainBeer
-        }
 
         fun bindData(domainBeer: Beer) {
-            binding.beerCardView.setBeerImage(domainBeer.beerImageUrl)
-            binding.beerCardView.setBeerName(domainBeer.beerName)
-            binding.beerCardView.setBeerVolume("${domainBeer.volume} ${domainBeer.volumeUnit}")
+            this.domainBeer = domainBeer
+            view.beerCardView.setBeerImage(domainBeer.beerImageUrl)
+            view.beerCardView.beerCardView.setBeerName(domainBeer.beerName)
+            view.beerCardView.setBeerVolume("${domainBeer.volume} ${domainBeer.volumeUnit}")
         }
 
-        init {
-            binding.beerCardView.setOnClickListener { beerItemClickListener.onBeerItemClicked(domainBeer) }
+        companion object {
+            fun fromParent(parent: ViewGroup): BeerListViewHolder {
+                val binding: BeerItemLayoutBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
+                        R.layout.beer_item_layout, parent, false)
+                return BeerListViewHolder(binding.root)
+            }
         }
     }
 
